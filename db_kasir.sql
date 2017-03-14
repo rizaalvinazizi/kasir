380dbda7d84defbe1f0ece5aaf239cad81b6b2d6
rizaalvin <rizaalvinazizi@gmail.com>
2017-03-14 23:20:55 +0700

    UTS

---
+++ db_kasir.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2017 at 02:46 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga_barang` int(11) NOT NULL,
+  `stock_barang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `harga_barang`, `stock_barang`) VALUES
+(1010, 'Buku Tulis', 30000, 100),
+(1011, 'Pensil', 3000, 100),
+(1100, 'Bolpoin', 2500, 100),
+(1101, 'Kertas Folio', 1000, 100),
+(1110, 'Tipe-X', 50000, 100),
+(1111, 'Penghapus', 1000, 100),
+(2002, 'Iqro''', 10000, 100),
+(2012, 'Lakban', 5000, 100),
+(2017, 'Al Quran', 10000, 100),
+(2019, 'Buku Sahih Muslim', 155000, 50),
+(2032, 'Juz Amma', 30000, 100),
+(2120, 'Parfum No Alkohol', 10000, 100),
+(4140, 'Al Quran Terjemahan', 200000, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_struk`
+--
+
+CREATE TABLE `tb_struk` (
+  `id_struk` int(11) NOT NULL,
+  `nama_kasir` varchar(50) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `jumlah harga` int(11) NOT NULL,
+  `diskon` varchar(10) NOT NULL,
+  `harga_diskon` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `nama_kasir` varchar(50) NOT NULL,
+  `barang` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `diskon` varchar(10) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`username`, `password`, `nama`) VALUES
+('elvirayu', 'aq1234', 'Elvira Ayu Sosilia'),
+('rizaalvin', 'aq1234', 'Riza Alvin Azizi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tb_struk`
+--
+ALTER TABLE `tb_struk`
+  ADD PRIMARY KEY (`id_struk`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
